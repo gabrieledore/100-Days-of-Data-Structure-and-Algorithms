76b6ed88667fddc6246cc3d736fc34cc3e544961
Gabriele Dore <gabrieledore1995@gmail.com>
2024-04-03 08:10:57 +0200

    Modified file

--- Day3a.java
+++ Day3a.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Day5 {
+public class Day3a {
     public static void main(String[] args) {
         int[] arr = generateArray();
         System.out.println(Arrays.toString(arr));
