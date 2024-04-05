6dfad60ffe07820350ebd90b48fc5db54c3bdf4f
Gabriele Dore <gabrieledore1995@gmail.com>
2024-04-05 19:15:10 +0200

    Insertion Sort

---
+++ Day5.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Day5 {
+    public static void main(String[] args) {
+        int[] arr = generateArray();
+        System.out.println(Arrays.toString(arr));
+        System.out.println("===============");
+        System.out.println(Arrays.toString(insertionSort(arr)));
+    }
+
+    public static int[] generateArray(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int length = scanner.nextInt();
+
+        System.out.println("Enter the minimum value of the array");
+        int min = scanner.nextInt();
+        System.out.println("Enter the maximum value of the array");
+        int max = scanner.nextInt();
+
+        int[] array = new int[length];
+        Random random = new Random();
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = random.nextInt(max - min + 1) + min;
+        }
+
+
+        return array;
+    }
+
+    public static int[] insertionSort(int[] array) {
+        for(int i = 1; i < array.length; i++){
+            int j = i-1;
+            while(j >= 0 && array[i] < array[j]){
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                System.out.println(Arrays.toString(array));
+                i--;
+                j--;
+            }
+            System.out.println("===");
+        }
+        return array;
+    }
+}
